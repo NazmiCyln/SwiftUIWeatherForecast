2c2423750a47dbf3cd77cd4f3000cb2abac2ae73
Nazmi Ceylan <nazmican50@gmail.com>
2024-08-29 09:49:30 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nazmi Ceylan on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
